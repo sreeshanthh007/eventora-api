df37463e2ea7930a64645942559eda4382db64db
sreesanth.ks <sreeshanthh007@gmail.com>
2025-12-10 16:27:36 +0530

    feat:(added readme file)

--- src/entities/controllerInterfaces/client/client-controller.interface.ts
+++ src/entities/controllerInterfaces/client/client-controller.interface.ts
@@ -1,28 +1,26 @@
 import { Request, Response } from "express";
 
-
-export interface IClientController{
-    refreshSession(req:Request,res:Response) : Promise<void>
-    updateProfileImage(req:Request,res:Response) : Promise<void>
-    updateProfileInformation(req:Request,res:Response) : Promise<void>
-    changePassword(req:Request,res:Response) : Promise<void>
-    getAllEvents(req:Request,res:Response) : Promise<void>
-    getAllCategories(req:Request,res:Response) : Promise<void>
-    getAllNotifications(req:Request,res:Response) : Promise<void>
-    markAsReadNotifications(req:Request,res:Response) : Promise<void>
-    getCategoriesForFilter(req:Request,res:Response) : Promise<void>
-    getAllEventsWithFilters(req:Request,res:Response) : Promise<void>
-    getAllServiceWithFilters(req:Request,res:Response) : Promise<void>
-    getEventDetails(req:Request,res:Response) : Promise<void>
-    getEventBooking(req:Request,res:Response) : Promise<void>
-    bookService(req:Request,res:Response) : Promise<void>
-    getBookedServices(req:Request,res:Response) : Promise<void>
-    getServiceDetails(req:Request,res:Response) : Promise<void>
-    getServicesProvidedByVendors(req:Request,res:Response) : Promise<void>
-    getClientWalletDetails(req:Request,res:Response) : Promise<void>
-    cancelTickets(req:Request,res:Response) : Promise<void>
-    cancelService(req:Request,res:Response) : Promise<void>
-    retryPaymentForTicket(req:Request,res:Response) : Promise<void>
-    getVendorWorkfolioForClient(req:Request,res:Response) : Promise<void>
-
+export interface IClientController {
+  refreshSession(req: Request, res: Response): Promise<void>;
+  updateProfileImage(req: Request, res: Response): Promise<void>;
+  updateProfileInformation(req: Request, res: Response): Promise<void>;
+  changePassword(req: Request, res: Response): Promise<void>;
+  getAllEvents(req: Request, res: Response): Promise<void>;
+  getAllCategories(req: Request, res: Response): Promise<void>;
+  getAllNotifications(req: Request, res: Response): Promise<void>;
+  markAsReadNotifications(req: Request, res: Response): Promise<void>;
+  getCategoriesForFilter(req: Request, res: Response): Promise<void>;
+  getAllEventsWithFilters(req: Request, res: Response): Promise<void>;
+  getAllServiceWithFilters(req: Request, res: Response): Promise<void>;
+  getEventDetails(req: Request, res: Response): Promise<void>;
+  getEventBooking(req: Request, res: Response): Promise<void>;
+  bookService(req: Request, res: Response): Promise<void>;
+  getBookedServices(req: Request, res: Response): Promise<void>;
+  getServiceDetails(req: Request, res: Response): Promise<void>;
+  getServicesProvidedByVendors(req: Request, res: Response): Promise<void>;
+  getClientWalletDetails(req: Request, res: Response): Promise<void>;
+  cancelTickets(req: Request, res: Response): Promise<void>;
+  cancelService(req: Request, res: Response): Promise<void>;
+  retryPaymentForTicket(req: Request, res: Response): Promise<void>;
+  getVendorWorkfolioForClient(req: Request, res: Response): Promise<void>;
 }
